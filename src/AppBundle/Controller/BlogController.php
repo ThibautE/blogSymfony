f6cf291c995687a3f12224c28a09db46dee1a338
ThibautE <thibaut07600@gmail.com>
2018-12-13 17:35:16 +0100

    Init project

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Blog\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post")
+     */
+    public function postAction(Request $request, $id)
+    {
+        $date = new \DateTime();
+        $article = new Post();
+        $article->setTitre("Mon premier article");
+        $article->setContenu("Voici mon premier article");
+        $article->setUrl("art".$date->getTimestamp());
+        $article->getDate(new \DateTime());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+        // replace this example code with whatever you need
+        return $this->render('default/post.html.twig', [
+            'var' => $id,
+        ]);
+    }
+
+}
